0de57b04bf435b780eda1d2a0bd903d446c96fba
mohit sharma <mohit.shar1005@gmail.com>
2019-04-16 22:57:33 +0530

    Adding styled components, responsive grid component

--- facenxt/src/components/App.js
+++ facenxt/src/components/App.js
@@ -1,9 +1,14 @@
 import React, { Component } from 'react'
+import { Row, Column } from './responsive';
 class App extends Component{
     render(){
         return(
             <div>
-                <h1>Welcome to My Starter App</h1>
+                <Row>
+                    <Column sm="10" xs="12">
+                        <h1>Welcome to My Starter App</h1>
+                    </Column>
+                </Row>
             </div>
         )
     }
